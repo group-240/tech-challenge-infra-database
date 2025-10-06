752cd8f59888618c0aa5c205d85053f70c5d03f9
Lima <andreluiz009@hotmail.com>
2025-10-06 05:13:16 -0300

    fix: usa hífen na senha conforme caracteres permitidos RDS

--- variables.tf
+++ variables.tf
@@ -26,5 +26,5 @@ variable "db_password" {
   description = "Password para o banco de dados RDS - HARDCODED para ambiente DEV (apenas estudo)"
   type        = string
   sensitive   = true
-  default     = "TechChallenge2024@Pass"
+  default     = "Tech-Challenge-2024"
 }
