374bbdd036c532cdccafe176f639f193b8b158c0
Lima <andreluiz009@hotmail.com>
2025-10-06 05:26:59 -0300

    fix password

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "aws_db_instance" "main" {
   # Database
   db_name  = var.db_name
   username = var.db_username
-  password = var.db_password
+  password = "DevEnvironment2024!"  # HARDCODED - Ambiente DEV apenas
 
   # Network
   db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
